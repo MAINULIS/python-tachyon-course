408b6a049eafdd83fcb7f3e72131be5b5990840e
Mainul Islam <mainulislam6168@gmail.com>
2026-04-06 09:56:35 +0600

    Multiplication Table using while loop

--- while-loop.py
+++ while-loop.py
@@ -2,3 +2,11 @@ i = 1
 while i < 11:
     print(i)
     i = i+2
+
+num = int(input("Enter a Number.. \n"))
+
+i = 1
+print(f"Multiplication Table for {num}:")
+while i <= 10:
+    print(f"{num} x {i} = {num * i}")
+    i += 1
